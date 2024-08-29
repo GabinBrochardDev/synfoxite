ffce49f4cf043f35af7ceac65e8f646537a93221
GabinBrochardDev <gbrochard.dev@gmail.com>
2024-08-29 13:48:00 +0200

    ppc OK 1347

--- src/Controller/RpsController.php
+++ src/Controller/RpsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Guirlande;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RpsController extends AbstractController
 {
     #[Route('/677569726c616e6465', name: 'app_rps')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
 
         $choices = ['Pierre', 'Papier', 'Ciseaux'];
@@ -18,8 +21,15 @@ class RpsController extends AbstractController
         $computerChoice = $choices[array_rand($choices)];
 
         $result = '';
+        $guirlandes = [];
+
         if ($userChoice) {
             $result = $this->determineWinner($userChoice, $computerChoice);
+
+            if ($result === 'Vous avez gagné !') {
+                // Récupérer les enregistrements de la table "guirlande"
+                $guirlandes = $entityManager->getRepository(Guirlande::class)->findAll();
+            }
         }
 
         return $this->render('rps/index.html.twig', [
@@ -27,6 +37,7 @@ class RpsController extends AbstractController
             'userChoice' => $userChoice,
             'computerChoice' => $computerChoice,
             'result' => $result,
+            'guirlandes' => $guirlandes,
         ]);
     }
 
